bdd406ebbc575b1cd6eae68ef5d0a85ef03b35c3
YuriSilenok <yuri.silenok@gmail.com>
2023-05-19 15:26:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/MyStack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp11
+{
+    internal class MyStack<T> : Stack<T>
+    {
+        /// <summary>
+        /// Задание 7. Создать стек заполненный элементами целого типа. 
+        /// Удалить из стека первый элемент и поместить его в вершину стека.
+        /// </summary>
+        public void FirstToLast()
+        {
+            if (Count == 0) return;
+            Stack<T> newStack = new Stack<T>();
+
+            //Убираем все элементы, кроме последнего в новый стек.
+            while (Count > 1)
+                newStack.Push(Pop());
+
+            //Записываем первый элемент в новую переменную.
+            T a = Pop();
+
+            //Возвращаем элементы в старый стек.
+            while (newStack.Count > 0)
+                Push(newStack.Pop());
+
+            Push(a);
+        }
+
+        /// <summary>
+        /// Задание 23. Создать стек из целых чисел. 
+        /// Поместить однозначные числа в один стек, двухзначные – в другой. 
+        /// Все остальные числа удалить из стека.
+        /// </summary>
+        /// <param name="OneSteck">Стек с однозначными числами</param>
+        /// <param name="DuStack">Стек с двузначными числами</param>
+        /// <exception cref="Exception"></exception>
+        public void Duplet(out MyStack<T> OneSteck, out MyStack<T> DuStack)
+        {
+            OneSteck = new MyStack<T>();
+            DuStack = new MyStack<T>();
+
+            Stack<T> tmp = new Stack<T>();
+
+            while (Count > 0)
+            {
+                T item = Pop();
+                string itemStr = item.ToString().Replace("-", "");
+                if (itemStr.Length > 2)
+                    continue;
+                else if (itemStr.Length == 1)
+                    OneSteck.Push(item);
+                else if (itemStr.Length == 2)
+                    DuStack.Push(item);
+                else
+                    throw new Exception("Разрыв континума");
+                tmp.Push(item);
+            }
+
+            while (tmp.Count > 0)
+                Push(tmp.Pop());
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", this);
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+
+
+        static MyStack<int> Zapolnenie(int a)
+        {
+            if (a <= 0)
+            {
+                throw new Exception("Введены неверные данные");
+            }
+            MyStack<int> newStack = new MyStack<int>();
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Введите число");
+                newStack.Push(int.Parse(Console.ReadLine()));
+            }
+            return newStack;
+        }
+
+        static void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1.Создать Стек");
+            Console.WriteLine("2.Вывести Стек");
+            Console.WriteLine("3.Поместить однозначные числа в один стек, двухзначные – в другой. Все остальные числа удалить из стека.");
+            Console.WriteLine("4.Удалить из стека первый элемент и поместить его в вершину стека.");
+            Console.WriteLine("Esc. Выход из программы");
+
+        }
+
+        static void Main()
+        {
+            PrintMenu();
+            MyStack<int> myStack = new MyStack<int>();
+            ConsoleKeyInfo K = new ConsoleKeyInfo();
+            do
+            {
+                try
+                {
+                    K = Console.ReadKey();
+                    switch (K.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            PrintMenu();
+                            break;
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Введите величину стека N");
+                                myStack = Zapolnenie(
+                                    int.Parse(
+                                        Console.ReadLine()));
+                                break;
+                            }
+                        case ConsoleKey.D2:
+                        case ConsoleKey.NumPad2:
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine(myStack);
+                                break;
+
+                            }
+                        case ConsoleKey.D3:
+                        case ConsoleKey.NumPad3:
+                            {
+                                Console.WriteLine();
+                                myStack.Duplet(out MyStack<int> o, out MyStack<int> d);
+
+                                Console.WriteLine("Стек с однозначными числами");
+                                Console.WriteLine(o);
+
+                                Console.WriteLine("Стек с двузначными числами");
+                                Console.WriteLine(d);
+
+                                Console.WriteLine("Исходный стек");
+                                Console.WriteLine(myStack);
+
+                                break;
+                            }
+                        case ConsoleKey.D4:
+                        case ConsoleKey.NumPad4:
+                            {
+                                Console.WriteLine();
+                                myStack.FirstToLast();
+                                Console.WriteLine(myStack);
+                                break;
+                            }
+                        default: break;
+                    }
+                }
+                catch (Exception obj)
+                {
+                    Console.WriteLine(obj.Message);
+                }
+            }
+            while (K.Key != ConsoleKey.Escape);
+        }
+
+    }
+}
